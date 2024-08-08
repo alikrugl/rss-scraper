b0719d4bd4239edd6d4db2b4a2ce122e079f4b0a
Alik Kruhlienia <alikrugl11@gmail.com>
2024-08-08 17:34:49 +0300

    add boilerplate code

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	godotenv.Load()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", handlerErr)
+
+	fmt.Println("Sever runnig on port", os.Getenv("PORT"))
+	http.ListenAndServe("127.0.0.1:"+os.Getenv("PORT"), mux)
 }
